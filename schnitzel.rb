f68738c4e34406c6ecf1446585ad078313fa9373
Thomas Smits <thomsmits@gmail.com>
2019-09-27 18:32:18 +0200

    Added missing quotation marks

--- schnitzel.rb
+++ schnitzel.rb
@@ -291,7 +291,7 @@ exercises << Exercise.new(
   %Q{
     Sie möchten eine Datei mit Ihre, Lieblingsfilm anlegen - der übrigens "Pulp Fiction" heißt.
 
-    Gehen Sie in das Verzeichnis "#{PROJEKT_PATH}" und lege dort eine Datei mit dem Namen film.txt an, in die Sie "Pulp Fiction" schreiben. Verwenden Sie hierzu entweder den Editor vi oder nano.
+    Gehen Sie in das Verzeichnis "#{PROJEKT_PATH}" und lege dort eine Datei mit dem Namen "film.txt" an, in die Sie "Pulp Fiction" schreiben. Verwenden Sie hierzu entweder den Editor vi oder nano.
   },
   :enter,
   'cd ; cd linux-kurs ; echo "Pulp Fiction" > film.txt',
