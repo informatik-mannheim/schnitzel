b64b18b7586818001c921cf5bdbaaef8566d67dc
Thomas Smits <thomsmits@gmail.com>
2019-10-08 17:45:05 +0200

    Allow calling the build script from the main directory

--- bin/build.rb
+++ bin/build.rb
@@ -3,13 +3,22 @@
 # Script to bundle all files together into one single
 # script `schnitzel`
 
+# Allow calling this script form the +bin+ folder
+# or its parent
+
+prefix = if __FILE__ =~ %r{bin/}
+           "."
+         else
+           ".."
+         end
+
 puts "Building `schnitzel`"
 
 # Join files together to the schnitzel script
-File.open('../schnitzel', 'w') do |out|
-    File.open('../src/schnitzel.rb').each_line do |line|
+File.open("#{prefix}/schnitzel", 'w') do |out|
+    File.open("#{prefix}/src/schnitzel.rb").each_line do |line|
     if line =~ /require_relative '(.*)'/
-	    out.puts File.read("../src/#{$1}")
+	    out.puts File.read("#{prefix}/src/#{$1}")
       out.puts ''
     else
       out.puts line
