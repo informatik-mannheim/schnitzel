0a7fc4b4d26b5848dee67c641e7a1912b48be302
AlpixTM <contact@alpix.eu>
2019-12-31 18:54:43 +0100

    New values based on the new generated chaos.tar.xz

--- src/exercises/15_tar.rb
+++ src/exercises/15_tar.rb
@@ -9,5 +9,5 @@
     },
     :enter,
     'tar -xJf chaos.tar.xz',
-    -> () { Dir.exists?("#{PATH}/chaos") && Dir["#{PATH}/chaos/**/*"].count == 66898 }
+    -> () { Dir.exists?("#{PATH}/chaos") && Dir["#{PATH}/chaos/**/*"].count == 66899 }
 )
