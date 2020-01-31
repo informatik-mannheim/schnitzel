7a12d9aa98290e938abed3549294e902f932b0ff
Thomas Smits <thomsmits@gmail.com>
2020-01-31 18:02:23 +0100

    Added exercise on du

---
+++ src/exercises/26_du.rb
@@ -0,0 +1,21 @@
+# df
+@exercises << Exercise.new(
+    'Es kommt doch auf die Größe an.',
+    'Machen Sie sich mit der Funktionsweise von "du" vertraut.',
+    %Q{
+      Nachdem Sie jetzt wissen, dass Sie auf Ihrer Festplatte noch etwas Platz für Ihre Schnitzelrezepte haben, stellt sich die Frage, wie groß die Rezepte jetzt schon sind.
+
+      Benutzen Sie das Kommando `du`, um herauszufinden, wie viel Platz alle Dateien im Ordner `#{PROJEKT_PATH}/chaos` zusammen verbrauchen.
+    },
+    'Wie viel Platz (in Megabyte) verbraucht der Ordner chaos?',
+    "du -h #{PROJEKT_PATH}/chaos",
+    -> (os) do
+       # Way to determine used space is really hacky but we would like
+       # to avoid pulling additional gems like sys/filesystem or similar
+       input == if os == :linux
+         `du -d0 -P -B M #{PATH}/chaos`.split(/\b/)[0].to_i.to_s
+       elsif os == :macos
+         `du -P -d0 -m #{PATH}/chaos`.split(/\b/)[0].to_i.to_s
+       end
+    end
+)
