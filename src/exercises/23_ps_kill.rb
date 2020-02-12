5750ba6e7abba35191beea1e5dbc7b72e6539305
AlpixTM <contact@alpix.eu>
2020-02-12 17:01:38 +0100

    :speech_balloon: Added that the PID must be behind kill

--- src/exercises/23_ps_kill.rb
+++ src/exercises/23_ps_kill.rb
@@ -6,7 +6,7 @@
       Auf Ihrem System läut ein Prozess mit dem Namen `"sleep"`. Finden Sie ihn und _beenden Sie den Prozess_. Möglicherweise laufen auch mehrere Prozesse, dann beenden Sie alle mit dem Namen `"sleep"`.
     },
     :enter,
-    'ps aux | grep sleep ; kill ...',
+    'ps aux | grep sleep ; kill PID',
     -> () { `ps aux | grep sleep | grep -v "grep"`.strip == "" },
     -> () { p = fork { exec "sleep 100000" }; Process.detach(p) }
 )
